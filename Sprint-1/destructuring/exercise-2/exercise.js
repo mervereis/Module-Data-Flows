10430581b9501f3aa8cfc6fdb03f79095c35439b
mervereis <124799990+mervereis@users.noreply.github.com>
2026-04-22 09:38:47 +0100

    Remove unused loops and console logs from exercise.js

--- Sprint-1/destructuring/exercise-2/exercise.js
+++ Sprint-1/destructuring/exercise-2/exercise.js
@@ -70,17 +70,3 @@ let hogwarts = [
     occupation: "Teacher",
   },
 ];
-
-for (const { firstName, lastName, house } of hogwarts) {
-  if (house === "Gryffindor") {
-    console.log(`${firstName} ${lastName}`);
-  }
-}
-console.log("Task1");
-
-for (const { firstName, lastName, occupation, pet } of hogwarts) {
-  if (occupation === "Teacher" && pet) {
-    console.log(`${firstName} ${lastName}`);
-  }
-}
-console.log("task2");
